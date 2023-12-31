2ba5c6d4e05b269d44a05a110916cb7053a9b7f5
unknown <saadallahibraim17@gmail.com>
2023-12-31 22:09:11 +0100

    fixing

--- MyProject-oop/phase2/connection.php
+++ MyProject-oop/phase2/connection.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$db_name = "vtu-db";
+$db_name = "VTU-DB";
 $con = mysqli_connect($server,$username,$password,$db_name);
 
 // if($connection){
